850b210c43055b3da121c85800648a31a116d6e0
sBouzols <sylvain.bouzols@gmail.com>
2021-08-13 17:02:56 +0200

    docs(): add License header where missing and update year where needed

--- src/utils/UIconstants.js
+++ src/utils/UIconstants.js
@@ -1,2 +1,9 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export const HORIZONTAL_SHIFT = 16;
 export const VERTICAL_SHIFT = -4;
