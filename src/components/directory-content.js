6d0d1b8f5bd69d56d0fa96b5e6fff38342a7eb6e
etiennehomer <etiennehomer@gmail.com>
2021-08-31 17:40:04 +0200

    Fix context menu icons (#38)
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/components/directory-content.js
+++ src/components/directory-content.js
@@ -41,6 +41,7 @@ import Menu from '@material-ui/core/Menu';
 import BuildIcon from '@material-ui/icons/Build';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import GetAppIcon from '@material-ui/icons/GetApp';
 import IconButton from '@material-ui/core/IconButton';
 
 import ExportDialog from './export-dialog';
@@ -681,12 +682,17 @@ const DirectoryContent = () => {
                                     )}
                                 </>
                             )}
-                            {makeMenuItem('export', handleOpenExportStudy)}
+                            {makeMenuItem(
+                                'export',
+                                handleOpenExportStudy,
+                                <GetAppIcon fontSize="small" />
+                            )}
                             {allowsDelete(true) && (
                                 <>
                                     {makeMenuItem(
                                         'delete',
-                                        handleOpenDeleteStudy
+                                        handleOpenDeleteStudy,
+                                        <DeleteIcon fontSize="small" />
                                     )}
                                 </>
                             )}
