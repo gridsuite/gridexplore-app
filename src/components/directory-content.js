f63046a5acc25064c271323dfe377337f73591d9
HOMER Etienne <etiennehomer@gmail.com>
2021-08-31 16:29:30 +0200

    Fix import order
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/components/directory-content.js
+++ src/components/directory-content.js
@@ -41,8 +41,8 @@ import Menu from '@material-ui/core/Menu';
 import BuildIcon from '@material-ui/icons/Build';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
 import GetAppIcon from '@material-ui/icons/GetApp';
+import IconButton from '@material-ui/core/IconButton';
 
 import ExportDialog from './export-dialog';
 import RenameDialog from './dialogs/rename-dialog';
