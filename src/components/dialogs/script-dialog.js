8229be86430215aed1f66bfb848a20f8876771a0
Etienne Homer <etiennehomer@gmail.com>
2021-12-07 17:48:26 +0100

    no http call when currentItemId is null
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/components/dialogs/script-dialog.js
+++ src/components/dialogs/script-dialog.js
@@ -124,34 +124,39 @@ const ScriptDialog = ({ id, open, onClose, onError, title, type }) => {
 
     const getCurrentScript = useCallback(
         (currentItemId) => {
-            if (type === ElementType.CONTINGENCY_LIST) {
-                getContingencyList(ContingencyListType.SCRIPT, currentItemId)
-                    .then((data) => {
-                        if (data) {
-                            setCurrentScript(data);
-                            setAceEditorContent(data.script);
-                            setName(data.name);
-                            setDescription(data.description);
-                        }
-                    })
-                    .catch((error) => {
-                        onError(error.message);
-                    });
-            } else if (type === ElementType.FILTER) {
-                getFilterById(currentItemId)
-                    .then((data) => {
-                        if (data) {
-                            setCurrentScript(data);
-                            setAceEditorContent(
-                                data.script === null ? '' : data.script
-                            );
-                            setName(data.name);
-                            setDescription(data.description);
-                        }
-                    })
-                    .catch((error) => {
-                        onError(error.message);
-                    });
+            if (currentItemId != null) {
+                if (type === ElementType.CONTINGENCY_LIST) {
+                    getContingencyList(
+                        ContingencyListType.SCRIPT,
+                        currentItemId
+                    )
+                        .then((data) => {
+                            if (data) {
+                                setCurrentScript(data);
+                                setAceEditorContent(data.script);
+                                setName(data.name);
+                                setDescription(data.description);
+                            }
+                        })
+                        .catch((error) => {
+                            onError(error.message);
+                        });
+                } else if (type === ElementType.FILTER) {
+                    getFilterById(currentItemId)
+                        .then((data) => {
+                            if (data) {
+                                setCurrentScript(data);
+                                setAceEditorContent(
+                                    data.script === null ? '' : data.script
+                                );
+                                setName(data.name);
+                                setDescription(data.description);
+                            }
+                        })
+                        .catch((error) => {
+                            onError(error.message);
+                        });
+                }
             }
         },
         [onError, type]
