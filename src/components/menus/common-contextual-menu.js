c617d848657258c31935f74f5629ab0645358cc5
sBouzols <sylvain.bouzols@gmail.com>
2022-03-22 10:57:40 +0100

    fix(Menu): getContentAnchorEl Menu prop is removed in V5
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/menus/common-contextual-menu.js
+++ src/components/menus/common-contextual-menu.js
@@ -15,7 +15,7 @@ const StyledMenu = withStyles({
     paper: {
         border: '1px solid #d3d4d5',
     },
-})((props) => <Menu elevation={0} getContentAnchorEl={null} {...props} />);
+})((props) => <Menu elevation={0} {...props} />);
 
 /**
  * Generic Contextual Menu View
