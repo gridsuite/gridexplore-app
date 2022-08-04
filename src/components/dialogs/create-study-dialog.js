d63e6466ccf6d7a078b3f7c54200fb95da5d2cc3
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2022-08-04 14:05:08 +0200

    fix: exported useMeta hook to commons-ui project
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/dialogs/create-study-dialog.js
+++ src/components/dialogs/create-study-dialog.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import makeStyles from '@mui/styles/makeStyles';
 import CheckIcon from '@mui/icons-material/Check';
@@ -51,18 +51,8 @@ import {
 import { ElementType } from '../../utils/elementType';
 import { useFileValue } from './field-hook';
 import { keyGenerator } from '../../utils/functions.js';
-import {
-    Autocomplete,
-    Chip,
-    Divider,
-    Grid,
-    List,
-    ListItem,
-    Stack,
-    Switch,
-    Tooltip,
-    Typography,
-} from '@mui/material';
+import { Divider, Grid, Stack } from '@mui/material';
+import { useMeta } from '@gridsuite/commons-ui';
 
 const useStyles = makeStyles((theme) => ({
     addIcon: {
@@ -70,10 +60,6 @@ const useStyles = makeStyles((theme) => ({
     },
     addButtonArea: {
         height: '136px',
-    },
-    paramListItem: {
-        justifyContent: 'space-between',
-        gap: theme.spacing(2),
     },
     paramDivider: {
         marginTop: theme.spacing(2),
@@ -137,130 +123,6 @@ const SelectCase = () => {
     );
 };
 
-function longestCommonPrefix(strs) {
-    if (!strs?.length) return '';
-    let prefix = strs.reduce((acc, str) =>
-        str.length < acc.length ? str : acc
-    );
-
-    for (let str of strs) {
-        while (str.slice(0, prefix.length) !== prefix) {
-            prefix = prefix.slice(0, -1);
-        }
-    }
-    return prefix;
-}
-
-const useMeta = (metasAsArray) => {
-    const classes = useStyles();
-    const longestPrefix = longestCommonPrefix(metasAsArray.map((m) => m.name));
-    const lastDotIndex = longestPrefix.lastIndexOf('.');
-    const prefix = longestPrefix.slice(0, lastDotIndex + 1);
-
-    const defaultInst = useMemo(() => {
-        return Object.fromEntries(
-            metasAsArray.map((m) => {
-                if (m.type === 'BOOLEAN') return [m.name, m.defaultValue];
-                if (m.type === 'STRING_LIST')
-                    return [m.name, m.defaultValue ?? []];
-                return [m.name, m.defaultValue ?? null];
-            })
-        );
-    }, [metasAsArray]);
-    const [inst, setInst] = useState(defaultInst);
-
-    const onBoolChange = (value, paramName) => {
-        setInst((prevInst) => {
-            const nextInst = { ...prevInst };
-            nextInst[paramName] = value;
-            return nextInst;
-        });
-    };
-
-    const onFieldChange = (value, paramName) => {
-        setInst((prevInst) => {
-            const nextInst = { ...prevInst };
-            nextInst[paramName] = value;
-            return nextInst;
-        });
-    };
-
-    const renderField = (meta) => {
-        switch (meta.type) {
-            case 'BOOLEAN':
-                return (
-                    <Switch
-                        checked={inst?.[meta.name] ?? defaultInst[meta.name]}
-                        onChange={(e) =>
-                            onBoolChange(e.target.checked, meta.name)
-                        }
-                    />
-                );
-            case 'STRING_LIST':
-                return (
-                    <Autocomplete
-                        fullWidth
-                        multiple
-                        options={meta.possibleValues ?? []}
-                        freeSolo={!meta.possibleValues}
-                        onChange={(e, value) => onFieldChange(value, meta.name)}
-                        value={inst?.[meta.name] ?? defaultInst[meta.name]}
-                        renderTags={(value, getTagProps) =>
-                            value.map((option, index) => (
-                                <Chip
-                                    variant="outlined"
-                                    label={option}
-                                    {...getTagProps({ index })}
-                                />
-                            ))
-                        }
-                        renderInput={(params) => (
-                            <TextField {...params} variant="standard" />
-                        )}
-                    />
-                );
-            default:
-                return (
-                    <TextField
-                        fullWidth
-                        defaultValue={
-                            inst?.[meta.name] ?? defaultInst[meta.name]
-                        }
-                        onChange={(e) =>
-                            onFieldChange(e.target.value, meta.name)
-                        }
-                        variant={'standard'}
-                    />
-                );
-        }
-    };
-
-    const resetValueToDefault = () => {
-        setInst({});
-    };
-
-    const comp = (
-        <List>
-            {metasAsArray.map((meta) => (
-                <Tooltip
-                    title={meta.description}
-                    enterDelay={1200}
-                    key={meta.name}
-                >
-                    <ListItem key={meta.name} className={classes.paramListItem}>
-                        <Typography style={{ minWidth: '30%' }}>
-                            {meta.name.slice(prefix.length)}
-                        </Typography>
-                        {renderField(meta)}
-                    </ListItem>
-                </Tooltip>
-            ))}
-        </List>
-    );
-
-    return [inst, comp, resetValueToDefault];
-};
-
 /**
  * Dialog to create a study
  * @param {Boolean} open Is the dialog open ?
@@ -319,7 +181,7 @@ export const CreateStudyDialog = ({ open, onClose, providedCase }) => {
             getCaseImportParameters(providedCase?.elementUuid).then(
                 (result) => {
                     // sort possible values alphabetically to display select options sorted
-                    result.parameters = result.parameters.map((p) => {
+                    result.parameters = result.parameters?.map((p) => {
                         let sortedPossibleValue = p.possibleValues?.sort(
                             (a, b) => a.localeCompare(b)
                         );
