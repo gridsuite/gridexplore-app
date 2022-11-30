d50e5fff5cd9940af3b8342b6b88e8d7fdfff764
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2022-11-30 11:08:01 +0100

    Revert "Fix Studies openning" (#176)
    * Revert "Fix Studies openning (#175)"
    
    This reverts commit 914dca94b34bcf73991e5fa04b7851250313fd6d.
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/directory-content.js
+++ src/components/directory-content.js
@@ -122,33 +122,40 @@ const DirectoryContent = () => {
     const [mousePosition, setMousePosition] =
         React.useState(initialMousePosition);
 
-    const handleRowClick = (event, index, data) => {
-        if (childrenMetadata[data.elementUuid] !== undefined) {
-            const subtype = childrenMetadata[data.elementUuid].subtype;
-            if (data.type === ElementType.STUDY) {
-                let url = getLink(data.elementUuid, data.type);
+    const handleRowClick = (event) => {
+        if (childrenMetadata[event.rowData.elementUuid] !== undefined) {
+            const subtype = childrenMetadata[event.rowData.elementUuid].subtype;
+            if (event.rowData.type === ElementType.STUDY) {
+                let url = getLink(
+                    event.rowData.elementUuid,
+                    event.rowData.type
+                );
                 url
                     ? window.open(url, '_blank')
                     : handleError(
                           intl.formatMessage(
                               { id: 'getAppLinkError' },
-                              { type: data.type }
+                              { type: event.rowData.type }
                           )
                       );
-            } else if (data.type === ElementType.CONTINGENCY_LIST) {
+            } else if (event.rowData.type === ElementType.CONTINGENCY_LIST) {
                 if (subtype === ContingencyListType.FORM) {
-                    setCurrentFiltersContingencyListId(data.elementUuid);
+                    setCurrentFiltersContingencyListId(
+                        event.rowData.elementUuid
+                    );
                     setOpenFiltersContingencyDialog(true);
                 } else if (subtype === ContingencyListType.SCRIPT) {
-                    setCurrentScriptContingencyListId(data.elementUuid);
+                    setCurrentScriptContingencyListId(
+                        event.rowData.elementUuid
+                    );
                     setOpenScriptContingencyDialog(true);
                 }
-            } else if (data.type === ElementType.FILTER) {
+            } else if (event.rowData.type === ElementType.FILTER) {
                 if (subtype === FilterType.MANUAL) {
-                    setCurrentManualFilterId(data.elementUuid);
+                    setCurrentManualFilterId(event.rowData.elementUuid);
                     setOpenEditManualFilterDialog(true);
                 } else if (subtype === FilterType.AUTOMATIC) {
-                    setCurrentAutomaticFilterId(data.elementUuid);
+                    setCurrentAutomaticFilterId(event.rowData.elementUuid);
                     setOpenGenericFilterDialog(true);
                 }
             }
