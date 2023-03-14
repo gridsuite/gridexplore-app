adae636d3bcf29fdb678e34bc422a94fd24cbbfe
BOUTIER Charly <charly.boutier@rte-france.com>
2023-03-14 10:59:26 +0100

    eslint
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/dialogs/create-contingency-list-dialog.js
+++ src/components/dialogs/create-contingency-list-dialog.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
