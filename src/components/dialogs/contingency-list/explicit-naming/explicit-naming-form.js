e1ca9bd668ecca1519d22379ebd4951d45d74284
Seddik Yengui <seddik.yengui@rte-france.com>
2023-07-07 17:20:06 +0200

    add state when fetching

--- src/components/dialogs/contingency-list/explicit-naming/explicit-naming-form.js
+++ src/components/dialogs/contingency-list/explicit-naming/explicit-naming-form.js
@@ -117,6 +117,7 @@ const ExplicitNamingForm = () => {
             suppressRowClickSelection
             defaultColDef={defaultColDef}
             alwaysShowVerticalScroll
+            minNumberOfRows={3}
             csvProps={{
                 fileName: intl.formatMessage({ id: 'contingencyListCreation' }),
                 fileHeaders: csvFileHeaders,
