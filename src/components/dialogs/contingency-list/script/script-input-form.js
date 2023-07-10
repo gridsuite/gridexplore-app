0a2ed1c97e4828ebac121f5f00983dd094ef68cb
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-07-10 15:01:15 +0200

    fix: moved css to class
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/dialogs/contingency-list/script/script-input-form.js
+++ src/components/dialogs/contingency-list/script/script-input-form.js
@@ -1,19 +1,26 @@
 import AceInput from '../../../utils/rhf-inputs/ace-input';
 import { SCRIPT } from '../../../utils/field-constants';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+    aceInput: {
+        minWidth: '650px',
+        minHeight: '450px',
+        marginTop: '4px',
+        flexGrow: 1,
+    },
+}));
 
 const ScriptInputForm = () => {
+    const classes = useStyles();
+
     return (
         <AceInput
             name={SCRIPT}
             placeholder="Insert your groovy script here"
             fontSize="18px"
             editorProps={{ $blockScrolling: true }}
-            style={{
-                minWidth: '650px',
-                minHeight: '450px',
-                marginTop: '4px',
-                flexGrow: 1,
-            }}
+            className={classes.aceInput}
         />
     );
 };
