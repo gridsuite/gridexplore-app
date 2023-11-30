79638a5e602fd43039292f3aecc4047975484eff
David SARTORI <david.sartori_externe@rte-france.com>
2023-11-30 15:23:42 +0100

    Multiple download instead of zip for cases

--- src/components/utils/caseUtils.ts
+++ src/components/utils/caseUtils.ts
@@ -1,32 +1,19 @@
-import JSZip from 'jszip';
 import { downloadCase, getCaseOriginalName } from '../../utils/rest-api';
-import { saveAs } from 'file-saver';
 
 export const downloadCases = async (uuids: string[]) => {
-    const zip = new JSZip();
-    const fileNames: { [fileName: string]: number } = {};
     for (const uuid of uuids) {
         const result = await downloadCase(uuid);
         let name = await getCaseOriginalName(uuid);
         const blob = await result.blob();
-
-        if (typeof name === 'string') {
-            if (fileNames[name]) {
-                zip.file(
-                    `${name.substring(0, name.length - 6)}(${fileNames[
-                        name
-                    ]++}).xiidm`,
-                    blob
-                );
-            } else {
-                fileNames[name] = 1;
-                zip.file(name, blob);
-            }
-        } else {
-            zip.file(`${uuid}.xiidm`, blob);
-        }
+        const href = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = href;
+        link.setAttribute(
+            'download',
+            typeof name === 'string' ? name : `${uuid}.xiidm`
+        );
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
-    zip.generateAsync({ type: 'blob' }).then((content) =>
-        saveAs(content, 'cases.zip')
-    );
 };
