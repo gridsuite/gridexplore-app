87d38b6ee14fca246a099a45b6cf99f42fffdf3c
Seddik Yengui <seddik.yengui@rte-france.com>
2024-01-03 13:30:17 +0100

    fix prettier

--- src/components/dialogs/description-modification/description-modification-dialogue.tsx
+++ src/components/dialogs/description-modification/description-modification-dialogue.tsx
@@ -58,7 +58,7 @@ const DescriptionModificationDialogue: FunctionComponent<
                 });
             });
         },
-        [elementUuid]
+        [elementUuid, snackError]
     );
 
     return (
