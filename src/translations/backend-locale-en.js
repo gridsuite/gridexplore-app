8cb742aa6129ccba4ea96d81505dd2ececd8cac9
Charly B <47325354+EstherDarkish@users.noreply.github.com>
2024-01-31 12:31:44 +0100

    Relocates some i18b keys in new files (#340)
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

---
+++ src/translations/backend-locale-en.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const backend_locale_en = {
+    DIE: 'ARCADE',
+    XIIDM: 'XIIDM',
+    CGMES: 'CGMES',
+    'PSS/E': 'PSS/E',
+    UCTE: 'UCTE',
+    MATPOWER: 'MATPOWER',
+    POWERFACTORY: 'POWERFACTORY',
+    'IEEE CDF': 'IEEE CDF',
+};
+
+export default backend_locale_en;
