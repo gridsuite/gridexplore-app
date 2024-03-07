1eac0635119cd889ccc37a7ea484b75f3bb07e6d
Seddik Yengui <seddik.yengui@rte-france.com>
2024-03-07 10:06:06 +0100

    fix checkstyle

--- src/components/dialogs/stashed-elements/stashed-elements-dialog.tsx
+++ src/components/dialogs/stashed-elements/stashed-elements-dialog.tsx
@@ -96,6 +96,7 @@ const StashedElementsDialog = ({
         directoryToRestore,
         onStashedElementChange,
         onClose,
+        intl,
     ]);
 
     const handleRestore = useCallback(() => {
