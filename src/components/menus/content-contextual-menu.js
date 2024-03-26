e4f837d6e80dd5a8a6959a01a0cf43a025c0d57a
Anis Touri <anistouri@gmail.com>
2024-03-26 17:24:16 +0100

    Group actions for modifying directory elements (#392)
    Signed-off-by: TOURI ANIS <anis-1.touri@rte-france.com>

--- src/components/menus/content-contextual-menu.js
+++ src/components/menus/content-contextual-menu.js
@@ -36,7 +36,7 @@ import {
     duplicateStudy,
     fetchElementsInfos,
     getNameCandidate,
-    moveElementToDirectory,
+    moveElementsToDirectory,
     newScriptFromFilter,
     newScriptFromFiltersContingencyList,
     renameElement,
@@ -335,7 +335,7 @@ const ContentContextualMenu = (props) => {
     );
 
     const [moveCB] = useMultipleDeferredFetch(
-        moveElementToDirectory,
+        moveElementsToDirectory,
         undefined,
         moveElementErrorToString,
         moveElementOnError,
@@ -671,14 +671,14 @@ const ContentContextualMenu = (props) => {
                         open={true}
                         onClose={(selectedDir) => {
                             if (selectedDir.length > 0) {
-                                moveCB(
-                                    selectedElements.map((element) => {
-                                        return [
-                                            element.elementUuid,
-                                            selectedDir[0].id,
-                                        ];
-                                    })
-                                );
+                                moveCB([
+                                    [
+                                        selectedElements.map(
+                                            (element) => element.elementUuid
+                                        ),
+                                        selectedDir[0].id,
+                                    ],
+                                ]);
                             }
                             handleCloseDialog();
                         }}
