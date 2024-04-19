15f77a1b614770dfb50158dd4d782d1b48c7688a
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-04-19 14:42:11 +0200

    add licence header

--- src/components/dialogs/search-item.js
+++ src/components/dialogs/search-item.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { getFileIcon } from '@gridsuite/commons-ui';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
