6e25e465627e737855faacd64721757d3c55fc74
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-06-11 16:47:07 +0200

    fix: unused import
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/search/search-item.tsx
+++ src/components/search/search-item.tsx
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { ElementType, getFileIcon } from '@gridsuite/commons-ui';
+import { getFileIcon } from '@gridsuite/commons-ui';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { FormattedMessage } from 'react-intl';
