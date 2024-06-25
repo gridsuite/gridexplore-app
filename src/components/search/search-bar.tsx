364c2d3e8e25502f208b76111eb0f89304721dcd
jamal-khey <myjamal89@gmail.com>
2024-06-25 18:06:09 +0200

    fix prettier

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -43,7 +43,9 @@ export const SearchBar: FunctionComponent<SearchBarProps> = ({ inputRef }) => {
     const { snackError } = useSnackMessage();
     const treeData = useSelector((state: ReduxState) => state.treeData);
     const treeDataRef = useRef<ITreeData>();
-    const selectedDirectory = useSelector((state: ReduxState) => state.selectedDirectory);
+    const selectedDirectory = useSelector(
+        (state: ReduxState) => state.selectedDirectory
+    );
     treeDataRef.current = treeData;
 
     const fetchElements: (
