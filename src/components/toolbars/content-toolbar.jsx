23ba583158a79c6258c4663e5c0d01b3726ac636
Abdelsalem <46495975+AbdelHedhili@users.noreply.github.com>
2024-08-22 07:53:12 +0200

    allow user to delete on creation studies (#493)
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/components/toolbars/content-toolbar.jsx
+++ src/components/toolbars/content-toolbar.jsx
@@ -101,7 +101,10 @@ const ContentToolbar = (props) => {
 
     const noCreationInProgress = useMemo(() => selectedElements.every((el) => el.hasMetadata), [selectedElements]);
 
-    const allowsDelete = useMemo(() => isUserAllowed && noCreationInProgress, [isUserAllowed, noCreationInProgress]);
+    const allowsDelete = useMemo(
+        () => isUserAllowed && selectedElements.every((el) => el.elementUuid != null),
+        [isUserAllowed, selectedElements]
+    );
 
     const allowsMove = useMemo(
         () =>
