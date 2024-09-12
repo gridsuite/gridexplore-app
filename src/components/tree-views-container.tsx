81cf9e0aa70b02e4c84877f59166205e5e847a85
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-09-12 09:59:33 +0200

    Fix missing directory rename/delete options on contextual menu
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/tree-views-container.tsx
+++ src/components/tree-views-container.tsx
@@ -590,7 +590,7 @@ const TreeViewsContainer = () => {
                               }
                             : undefined
                     }
-                    restrictMenuItems={true}
+                    restrictMenuItems={false}
                 />
             </div>
         </>
