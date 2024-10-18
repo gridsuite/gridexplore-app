39b1a0ad54f7f87af73aebb219e3662a895039ec
BOUTIER Charly <charly.boutier@rte-france.com>
2024-10-18 12:32:48 +0200

    Fix menu icon button hidden with a small screen
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/tree-views-container.tsx
+++ src/components/tree-views-container.tsx
@@ -612,6 +612,7 @@ const TreeViewsContainer = () => {
                     display: 'flex',
                     flexDirection: 'column',
                     height: '100%',
+                    width: '100%',
                     flexGrow: 1,
                 }}
                 onContextMenu={(e) => onContextMenu(e, undefined, 'anchorPosition')}
