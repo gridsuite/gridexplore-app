ce222f9f04642e0e6b648caf163db68ab393d62e
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-10-28 20:40:32 +0100

    fix licence header

--- src/utils/cyrb53.ts
+++ src/utils/cyrb53.ts
@@ -1,4 +1,8 @@
-/* cyrb53 (c) 2018 bryc (github.com/bryc)
+/* cyrb53 (c) 2018, bryc (github.com/bryc)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
  * License: Public domain (or MIT if needed). Attribution appreciated.
  */
 
