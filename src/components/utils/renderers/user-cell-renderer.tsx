41ad4a2d996128e3e78741c9d5c6c24f0b99eab8
sBouzols <sylvain.bouzols@gmail.com>
2024-11-12 17:35:40 +0100

    docs review
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/utils/renderers/user-cell-renderer.tsx
+++ src/components/utils/renderers/user-cell-renderer.tsx
@@ -7,7 +7,8 @@
 import { Avatar, Box, Theme, Tooltip } from '@mui/material';
 
 function getAbbreviationFromUserName(name: string) {
-    if (name == null /* null or undefined */ || name.trim() === '') {
+    /* null or undefined or only whitespaces */
+    if (name == null || name.trim() === '') {
         return '';
     }
     const splittedName = name.split(' ');
