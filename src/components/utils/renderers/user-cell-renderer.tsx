573d7e4ed8a5a645592db8dc161558c0bc2b1c5f
sBouzols <sylvain.bouzols@gmail.com>
2024-11-12 17:34:31 +0100

    docs(): add null or undefined comment
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/utils/renderers/user-cell-renderer.tsx
+++ src/components/utils/renderers/user-cell-renderer.tsx
@@ -7,7 +7,7 @@
 import { Avatar, Box, Theme, Tooltip } from '@mui/material';
 
 function getAbbreviationFromUserName(name: string) {
-    if (name == null || name.trim() === '') {
+    if (name == null /* null or undefined */ || name.trim() === '') {
         return '';
     }
     const splittedName = name.split(' ');
