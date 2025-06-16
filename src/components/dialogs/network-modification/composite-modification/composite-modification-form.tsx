9a4feff763d3c0e8bcb0b1c2c07c267167924a71
Ayoub LABIDI <ayoub.labidi@protonmail.com>
2025-06-16 17:41:06 +0200

    Fix composite modification dialog
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

--- src/components/dialogs/network-modification/composite-modification/composite-modification-form.tsx
+++ src/components/dialogs/network-modification/composite-modification/composite-modification-form.tsx
@@ -5,7 +5,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { UniqueNameInput, ElementType, FieldConstants } from '@gridsuite/commons-ui';
+import {
+    UniqueNameInput,
+    ElementType,
+    FieldConstants,
+    DescriptionField,
+    unscrollableDialogStyles,
+} from '@gridsuite/commons-ui';
 import { useSelector } from 'react-redux';
 import { AppState } from 'redux/types';
 import Box from '@mui/material/Box';
@@ -13,13 +19,18 @@ import Box from '@mui/material/Box';
 export default function CompositeModificationForm() {
     const activeDirectory = useSelector((state: AppState) => state.activeDirectory);
     return (
-        <Box>
-            <UniqueNameInput
-                name={FieldConstants.NAME}
-                label="nameProperty"
-                elementType={ElementType.MODIFICATION}
-                activeDirectory={activeDirectory}
-            />
-        </Box>
+        <>
+            <Box sx={unscrollableDialogStyles.unscrollableHeader}>
+                <UniqueNameInput
+                    name={FieldConstants.NAME}
+                    label="nameProperty"
+                    elementType={ElementType.MODIFICATION}
+                    activeDirectory={activeDirectory}
+                />
+            </Box>
+            <Box sx={unscrollableDialogStyles.unscrollableHeader}>
+                <DescriptionField />
+            </Box>
+        </>
     );
 }
