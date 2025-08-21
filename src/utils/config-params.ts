d23d6a00ed8440b3d0831f2fa6775f9575f19046
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-08-21 16:29:01 +0200

    fix: use commons-ui config params api (app specific)
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/utils/config-params.ts
+++ src/utils/config-params.ts
@@ -5,18 +5,4 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { LAST_SELECTED_DIRECTORY, PARAM_DEVELOPER_MODE, PARAM_LANGUAGE, PARAM_THEME } from '@gridsuite/commons-ui';
-
-export const COMMON_APP_NAME = 'common';
-export const APP_NAME = 'Explore';
-
-const COMMON_CONFIG_PARAMS_NAMES = new Set([
-    PARAM_THEME,
-    PARAM_LANGUAGE,
-    LAST_SELECTED_DIRECTORY,
-    PARAM_DEVELOPER_MODE,
-]);
-
-export function getAppName(paramName: string) {
-    return COMMON_CONFIG_PARAMS_NAMES.has(paramName) ? COMMON_APP_NAME : APP_NAME;
-}
+export const APP_NAME = 'explore';
