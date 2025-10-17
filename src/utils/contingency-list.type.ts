86865ce5ac725d9f338986e2cae0fb66015f4fc3
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2025-10-17 13:00:17 +0200

    Add filter by substation or VL to contingency list (#741)
    Introduced support for sub-equipment management in filter-based contingency lists.
    Added FilterBasedContingencyListVisualizationPanel for enhanced data visualization and management.

--- src/utils/contingency-list.type.ts
+++ src/utils/contingency-list.type.ts
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { UUID } from 'crypto';
+import { UUID } from 'node:crypto';
 
 // reproduce partially dto from Filter-server
 export interface FilterAttributes {
@@ -27,4 +27,30 @@ export interface FilteredIdentifiables {
 // type taken from actions-server
 export interface FilterBasedContingencyList {
     filters: FilterAttributes[];
+    selectedEquipmentTypesByFilter: Array<{
+        filterId: string;
+        equipmentTypes: string[];
+    }>;
+}
+
+// type taken from filter-server, for now it's the same as FilterBasedContingencyList
+export type FiltersWithEquipmentTypes = FilterBasedContingencyList;
+
+export interface FilterElement {
+    id: UUID;
+    name: string;
+    specificMetadata: {
+        equipmentType: string;
+    };
+}
+
+export enum ContingencyFieldConstants {
+    SUB_EQUIPMENT_TYPES_BY_FILTER = 'subEquipmentTypesByFilter',
+    FILTER_ID = 'filterId',
+    SUB_EQUIPMENT_TYPES = 'subEquipmentTypes',
+}
+
+export interface FilterSubEquipments {
+    [ContingencyFieldConstants.FILTER_ID]: string;
+    [ContingencyFieldConstants.SUB_EQUIPMENT_TYPES]: string[];
 }
