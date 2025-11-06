ae61cb362ed09b9545ee5ed7ac412206b20a701b
Rehili Ghazwa <ghazwarhili@gmail.com>
2025-11-06 13:44:16 +0100

    add persist

--- src/components/utils/downloadUtils.ts
+++ src/components/utils/downloadUtils.ts
@@ -122,6 +122,7 @@ export function useDownloadUtils() {
                         { id: 'export.message.started' },
                         { fileName: fileName || caseElement.elementName }
                     ),
+                    persist: true,
                 });
             } catch (error: any) {
                 if (error.name === 'AbortError') {
