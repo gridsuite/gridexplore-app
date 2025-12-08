7f9e4000abe9d2d3631fd139637a0f3e599e1cae
dbraquart <107846716+dbraquart@users.noreply.github.com>
2025-12-08 10:38:57 +0100

    Avoid element edition when no write permission (#778)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/utils/renderers/name-cell-renderer.tsx
+++ src/components/utils/renderers/name-cell-renderer.tsx
@@ -5,33 +5,35 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 import type { UUID } from 'node:crypto';
-import { IntlShape, useIntl } from 'react-intl';
-import { Box, CircularProgress } from '@mui/material';
+import { FormattedMessage, IntlShape, useIntl } from 'react-intl';
+import { Box, CircularProgress, Tooltip } from '@mui/material';
+import { Lock as LockIcon } from '@mui/icons-material';
 import {
     type ElementAttributes,
     ElementType,
     getFileIcon,
+    mergeSx,
     type MuiStyles,
     OverflowableText,
 } from '@gridsuite/commons-ui';
 
-const isElementCaseOrStudy = (objectType: ElementType) =>
-    objectType === ElementType.STUDY || objectType === ElementType.CASE;
+const waitingForAsyncCreation = (metadata: ElementAttributes, objectType: ElementType) =>
+    !metadata && (objectType === ElementType.STUDY || objectType === ElementType.CASE);
 
 function getDisplayedElementName(
     data: ElementAttributes,
     childrenMetadata: Record<UUID, ElementAttributes>,
     intl: IntlShape
 ) {
-    const { elementName, uploading, elementUuid } = data;
+    const { elementName, uploading, elementUuid, type } = data;
     const { formatMessage } = intl;
     if (uploading) {
         return `${elementName}\n${formatMessage({ id: 'uploading' })}`;
     }
-    if (!childrenMetadata[elementUuid]) {
+    if (waitingForAsyncCreation(childrenMetadata[elementUuid], type)) {
         return `${elementName}\n${formatMessage({ id: 'creationInProgress' })}`;
     }
-    return childrenMetadata[elementUuid].elementName;
+    return childrenMetadata[elementUuid]?.elementName ?? elementName;
 }
 
 const styles = {
@@ -40,48 +42,58 @@ const styles = {
         display: 'flex',
         alignItems: 'center',
     },
-    circularRoot: (theme) => ({
-        marginRight: theme.spacing(1),
-    }),
     icon: (theme) => ({
         marginRight: theme.spacing(1),
         width: '18px',
+        minWidth: '18px',
         height: '18px',
+        minHeight: '18px',
     }),
+    right: {
+        marginLeft: 'auto',
+    },
     tooltip: {
         maxWidth: '1000px',
     },
-    overflow: {
+    waitingName: {
         display: 'inline-block',
         whiteSpace: 'pre',
-        textOverflow: 'ellipsis',
         overflow: 'hidden',
         lineHeight: 'initial',
         verticalAlign: 'middle',
+    },
+    singleName: {
+        marginTop: 1,
     },
 } as const satisfies MuiStyles;
 
 export type NameCellRendererProps = {
     data: ElementAttributes;
     childrenMetadata: Record<UUID, ElementAttributes>;
+    directoryWritable: boolean;
 };
 
-export function NameCellRenderer({ data, childrenMetadata }: Readonly<NameCellRendererProps>) {
+export function NameCellRenderer({ data, childrenMetadata, directoryWritable }: Readonly<NameCellRendererProps>) {
     const intl = useIntl();
+    const metadata = childrenMetadata[data.elementUuid];
+    const waiting = waitingForAsyncCreation(metadata, data.type);
     return (
         <Box sx={styles.tableCell}>
             {/*  Icon */}
-            {!childrenMetadata[data.elementUuid] && isElementCaseOrStudy(data.type) && (
-                <CircularProgress size={18} sx={styles.circularRoot} />
-            )}
-            {childrenMetadata[data.elementUuid] && getFileIcon(data.type, styles.icon)}
+            {waiting && <CircularProgress size={18} sx={styles.icon} />}
+            {metadata && getFileIcon(data.type, styles.icon)}
             {/* Name */}
             <OverflowableText
                 text={getDisplayedElementName(data, childrenMetadata, intl)}
                 tooltipSx={styles.tooltip}
-                style={styles.overflow}
+                style={waiting ? styles.waitingName : styles.singleName}
                 data-testid="ElementName"
             />
+            {!directoryWritable && (
+                <Tooltip title={<FormattedMessage id="protectedElement" />}>
+                    <LockIcon sx={mergeSx(styles.icon, styles.right)} />
+                </Tooltip>
+            )}
         </Box>
     );
 }
