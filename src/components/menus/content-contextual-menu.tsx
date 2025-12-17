008cdc645951674fcaae61b8ab16c0cb43813ed0
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2025-12-17 17:56:15 +0100

    Bump commons-ui 147 (#786)
    Rename "enableDeveloperMode" to "isDeveloperMode" across the codebase
    Also include #783

--- src/components/menus/content-contextual-menu.tsx
+++ src/components/menus/content-contextual-menu.tsx
@@ -84,7 +84,7 @@ export default function ContentContextualMenu(props: Readonly<ContentContextualM
     const [directoryWritable, setDirectoryWritable] = useState(false);
     const [directoryReadable, setDirectoryReadable] = useState(false);
     const [permissionsLoaded, setPermissionsLoaded] = useState(false);
-    const [enableDeveloperMode] = useParameterState(PARAM_DEVELOPER_MODE);
+    const [isDeveloperMode] = useParameterState(PARAM_DEVELOPER_MODE);
 
     const { snackError } = useSnackMessage();
 
@@ -432,7 +432,7 @@ export default function ContentContextualMenu(props: Readonly<ContentContextualM
             });
         }
 
-        if (enableDeveloperMode && allowsExportCase()) {
+        if (isDeveloperMode && allowsExportCase()) {
             menuItems.push({
                 messageDescriptorId: 'download.export.button',
                 callback: () => handleOpenDialog(DialogsId.EXPORT),
@@ -477,7 +477,7 @@ export default function ContentContextualMenu(props: Readonly<ContentContextualM
         directoryReadable,
         isSingleElement,
         allowsDownload,
-        enableDeveloperMode,
+        isDeveloperMode,
         allowsExportCase,
         allowsSpreadsheetCollection,
         allowsConvertFilterIntoExplicitNaming,
@@ -554,6 +554,7 @@ export default function ContentContextualMenu(props: Readonly<ContentContextualM
                         activeDirectory={activeDirectory}
                         language={languageLocal}
                         filterType={FilterType.EXPLICIT_NAMING}
+                        isDeveloperMode={isDeveloperMode}
                     />
                 );
             case DialogsId.CREATE_SPREADSHEET_COLLECTION:
