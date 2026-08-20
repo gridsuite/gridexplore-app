bb476330b6e10edf9736661f4e3da4a93e367890
dbraquart <107846716+dbraquart@users.noreply.github.com>
2026-08-20 10:35:52 +0200

    chore(TwoWindingsTransformerCreation): use form from commons-UI (#939)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/dialogs/network-modification/composite-modification/composite-modification-dialog.tsx
+++ src/components/dialogs/network-modification/composite-modification/composite-modification-dialog.tsx
@@ -97,6 +97,10 @@ import {
     modificationByFormulaDtoToForm,
     modificationByFormulaFormToDto,
     ModificationByFormulaForm,
+    TwoWindingsTransformerForm,
+    twoWindingsTransformerCreationFormSchema,
+    twoWindingsTransformerCreationDtoToForm,
+    twoWindingsTransformerCreationFormToDto,
     staticVarCompensatorCreationFormSchema,
     staticVarCompensatorDtoToForm,
     staticVarCompensatorCreationFormToDto,
@@ -134,6 +138,7 @@ type SpecificModificationDialogProps = Pick<
     | 'ModificationForm'
     | 'isModification'
     | 'removeOptional'
+    | 'dialogWidth'
     | 'getExtraFormProps'
 >;
 
@@ -249,6 +254,20 @@ export default function CompositeModificationDialog({
                         titleId: 'ModifySubstation',
                         ModificationForm: SubstationModificationForm,
                         removeOptional: true,
+                    },
+                ],
+                [
+                    ModificationType.TWO_WINDINGS_TRANSFORMER_CREATION,
+                    {
+                        formSchema: twoWindingsTransformerCreationFormSchema,
+                        dtoToForm: twoWindingsTransformerCreationDtoToForm,
+                        formToDto: twoWindingsTransformerCreationFormToDto,
+                        errorHeaderId: 'TwoWindingsTransformerCreationError',
+                        titleId: 'CreateTwoWindingsTransformer',
+                        ModificationForm: TwoWindingsTransformerForm,
+                        isModification: false,
+                        removeOptional: false,
+                        dialogWidth: 'xl', // for steps table
                     },
                 ],
                 [
