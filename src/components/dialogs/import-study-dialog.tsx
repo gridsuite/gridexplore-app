8007bb662ce2be5099f39cf10a320f48b73ec158
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-09-17 15:16:45 +0200

    enhance import export study

--- src/components/dialogs/import-study-dialog.tsx
+++ src/components/dialogs/import-study-dialog.tsx
@@ -27,8 +27,8 @@ import { FieldValues, useController, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { AppState, UploadingElement } from '../../redux/types';
-import { importStudy } from '../../utils/rest-api';
-import { removeUploadingElement } from '../../redux/actions';
+import { importStudy, insertDirectory } from '../../utils/rest-api';
+import { addUploadingElement, removeUploadingElement } from '../../redux/actions';
 import PrefilledNameInput from './commons/prefilled-name-input';
 
 interface ImportStudyDialogProps {
@@ -96,15 +96,16 @@ export default function ImportStudyDialog({ open, onClose }: Readonly<ImportStud
 
     const handleImportStudy = useCallback(
         async (data: FieldValues) => {
-            if (!selectedDirectory?.elementUuid) {
+            if (!selectedDirectory?.elementUuid || !userId) {
                 snackError({ headerId: 'studyImportError' });
                 return;
             }
             const studyName = data[FieldConstants.NAME];
+            const newDirectory = await insertDirectory(studyName, selectedDirectory.elementUuid, userId);
             const uploadingStudy: UploadingElement = {
                 id: keyGenerator()(),
                 elementName: studyName,
-                directory: selectedDirectory.elementUuid,
+                directory: newDirectory.elementUuid,
                 type: ElementType.STUDY,
                 owner: userId,
                 lastModifiedBy: userId,
@@ -115,10 +116,9 @@ export default function ImportStudyDialog({ open, onClose }: Readonly<ImportStud
                 studyName,
                 data[FieldConstants.DESCRIPTION],
                 data.studyFiles?.[0] as File,
-                selectedDirectory.elementUuid
+                newDirectory.elementUuid
             )
                 .then(() => {
-                    dispatch(removeUploadingElement(uploadingStudy));
                     onClose();
                 })
                 .catch((error) => {
@@ -130,6 +130,7 @@ export default function ImportStudyDialog({ open, onClose }: Readonly<ImportStud
                         },
                     });
                 });
+            dispatch(addUploadingElement(uploadingStudy));
         },
         [dispatch, onClose, selectedDirectory?.elementUuid, snackError, userId]
     );
